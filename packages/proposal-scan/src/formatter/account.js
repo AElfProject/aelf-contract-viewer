5e4fc5ffa81b9062e84408fc05e30fad3cd0a740
hzz780 <hzz780@qq.com>
2022-03-09 10:44:14 +0800

    ci: 🎡 update aelf-sdk version;  check token symbol;

--- packages/proposal-scan/src/formatter/account.js
+++ packages/proposal-scan/src/formatter/account.js
@@ -149,6 +149,8 @@ async function tokenBalanceChangedFormatter(transaction, type) {
   // if (supplyChanged.length > 0) {
   //   await changeSupply(supplyChanged.map(v => v.symbol));
   // }
+  // TODO: 用来排除非法的Token名称或者用户自定义的Token名称
+  addressSymbols = addressSymbols.filter(v => v.symbol.match(/^[a-z0-9]+$/i));
   addressSymbols = lodash.uniq(addressSymbols.map(v => `${v.owner}_${v.symbol}`));
   const balancesFromCache = addressSymbols
     .filter(v => type !== QUERY_TYPE.LOOP && BALANCES_NOT_IN_LOOP[v] !== undefined)
